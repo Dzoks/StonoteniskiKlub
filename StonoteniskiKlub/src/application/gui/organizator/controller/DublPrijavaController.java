d85946b7fa0d463f443ff63ff995d1f442cae770
Dejan Gajic <dejan.gajic1110@gmail.com>
2017-12-06 16:34:25 +0100

    prvi commit

---
+++ StonoteniskiKlub/src/application/gui/organizator/controller/DublPrijavaController.java
@@ -0,0 +1,14 @@
+package application.gui.organizator.controller;
+import java.net.URL;
+import java.util.ResourceBundle;
+import application.gui.controller.*;
+
+public class DublPrijavaController extends BaseController{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
