40de29caea4b328add509461190f7083ebddf991
Dejan Gajic <dejan.gajic1110@gmail.com>
2017-12-09 15:54:08 +0100

    dodano dto

---
+++ StonoteniskiKlub/src/application/model/dao/KategorijaTurniraDAO.java
@@ -0,0 +1,37 @@
+package application.model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import application.model.dto.KategorijaTurniraDTO;
+import application.util.ConnectionPool;
+
+public class KategorijaTurniraDAO {
+	private final static String SQL_GET_BY_ID="select * from TURNIR_KATEGORIJA where Id=?";
+	
+	public static KategorijaTurniraDTO getById(int id){
+		KategorijaTurniraDTO retVal=new KategorijaTurniraDTO();
+		Connection c=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		
+		try {
+			c=ConnectionPool.getInstance().checkOut();
+			String query=SQL_GET_BY_ID;
+			Object pom[] = { id };
+			
+			ps=ConnectionPool.prepareStatement(c, query, false, pom);
+			rs=ps.executeQuery();
+			retVal=new KategorijaTurniraDTO(id, rs.getString("Kategorija"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			ConnectionPool.close(rs, ps);
+			ConnectionPool.getInstance().checkIn(c);
+		}
+		
+		return retVal;
+	}
+}
---
+++ StonoteniskiKlub/src/application/model/dto/UcesnikPrijavaDTO.java
@@ -0,0 +1,69 @@
+package application.model.dto;
+
+import java.sql.Date;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class UcesnikPrijavaDTO extends OsobaDTO{
+	private IntegerProperty idPrijave;
+	private IntegerProperty idTurnira;
+	private IntegerProperty idKategorije;
+	private Date datum;
+	
+	public UcesnikPrijavaDTO() {
+		super();
+	}
+
+	public UcesnikPrijavaDTO(Integer idPrijave, Integer idTurnira, Integer idKategorije, Date datum) {
+		super();
+		this.idPrijave = idPrijave==null ? null : new SimpleIntegerProperty(idPrijave);
+		this.idTurnira = idTurnira==null ? null : new SimpleIntegerProperty(idTurnira);
+		this.idKategorije = idKategorije==null ? null : new SimpleIntegerProperty(idKategorije);
+		this.datum = datum;
+	}
+	
+	public final IntegerProperty idTurniraProperty() {
+		return this.idTurnira;
+	}
+	
+
+	public final int getIdTurnira() {
+		return this.idTurniraProperty().get();
+	}
+	
+
+	public final void setIdTurnira(final int idTurnira) {
+		this.idTurniraProperty().set(idTurnira);
+	}
+	
+
+	public final IntegerProperty idKategorijeProperty() {
+		return this.idKategorije;
+	}
+	
+
+	public final int getIdKategorije() {
+		return this.idKategorijeProperty().get();
+	}
+	
+
+	public final void setIdKategorije(final int idKategorije) {
+		this.idKategorijeProperty().set(idKategorije);
+	}
+
+	public final IntegerProperty idPrijaveProperty() {
+		return this.idPrijave;
+	}
+	
+
+	public final int getIdPrijave() {
+		return this.idPrijaveProperty().get();
+	}
+	
+
+	public final void setIdPrijave(final int idPrijave) {
+		this.idPrijaveProperty().set(idPrijave);
+	}
+	
+}
