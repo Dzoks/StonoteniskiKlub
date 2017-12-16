18491571ee3f2e35c8a320db84c10bf0f349d6ad
Helena Brkanlic <hheell>
2017-12-16 21:47:02 +0100

    Napravljena vecina GUIja

--- StonoteniskiKlub/src/application/Main.java
+++ StonoteniskiKlub/src/application/Main.java
@@ -3,12 +3,12 @@ package application;
 import application.gui.controller.BaseController;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+import application.gui.racunovodja.controller.PocetniProzorRacunovodjaController;
 public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		BaseController.changeScene("/application/gui/organizator/view/TurniriView.fxml", primaryStage);
+		BaseController.changeScene("/application/gui/racunovodja/view/PocetniProzorRacunovodja.fxml", primaryStage);
 		primaryStage.setResizable(false);
 		primaryStage.setTitle("Turniri");
 		primaryStage.show();
